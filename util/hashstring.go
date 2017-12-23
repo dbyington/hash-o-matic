434e1e0d1f2b9267fdbbc1a289a590120d4aedf4
Don Byington <don@dbyington.com>
2017-12-23 15:48:53 -0700

    feat: String hashing

---
+++ util/hashstring.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+    "crypto/sha512"
+    "encoding/base64"
+)
+
+func HashString(stringToHash string) string {
+
+    // Sum512() argument must be type [size]byte
+    byteArray := []byte(stringToHash)
+    sum512byteArray := sha512.Sum512(byteArray)
+
+    // EncodeToString() argument must be type []byte so first turn the byte array into a string
+    sum512string := string(sum512byteArray[:])
+    base64string := base64.StdEncoding.EncodeToString([]byte(sum512string))
+
+    return base64string
+}
