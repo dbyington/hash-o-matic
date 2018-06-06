73e3e2a26edc75cd2e20baf302bb19fdbc4af751
Don Byington <don@dbyington.com>
2018-06-05 20:53:39 -0600

    add redirect handler

--- hash-o-matic.go
+++ hash-o-matic.go
@@ -22,6 +22,11 @@ var (
 	Hashes        map[int64]string
 )
 
+type redirect struct {
+	target string
+	code   int
+}
+
 func main() {
 
 	serverAddress := DEFAULT_LISTEN_ADDR
@@ -32,6 +37,7 @@ func main() {
 	Hashes = make(map[int64]string)
 
 	// handlers for configured endpoints
+	http.HandleFunc("/", RedirectHandler)
 	http.HandleFunc("/hash", HashPostHandler)
 	http.HandleFunc("/hash/", HashGetHandler)
 	http.HandleFunc("/shutdown", ShutdownHandler)
@@ -117,4 +123,37 @@ func LogHandler(handler http.Handler) http.Handler {
 		handler.ServeHTTP(res, req)
 		log.Printf("%s Request from %s Complete\n", req.Method, req.RemoteAddr)
 	})
+}
+
+func getRedirectTargets() (redirectMap map[string]redirect) {
+	redirectMap = make(map[string]redirect)
+	redirectMap["/"] = redirect{
+		"https://github.com/dbyington/hash-o-matic#readme",
+		http.StatusFound,
+	}
+
+	return redirectMap
+}
+
+func mapRedirect(requestUrl string) (redirectUrl redirect) {
+
+	var redir redirect
+	var err error
+	var ok bool
+	redirectMap := getRedirectTargets()
+
+	if redir, ok = redirectMap[requestUrl]; ok {
+		if err != nil {
+			log.Printf("Error parsing redirect url %s, got %s", redir.target, err.Error())
+		}
+	}
+	return redir
+}
+
+func RedirectHandler(res http.ResponseWriter, req *http.Request) {
+
+	redirectTo := mapRedirect(req.RequestURI)
+	if redirectTo.code > 0 {
+		http.Redirect(res, req, redirectTo.target, redirectTo.code)
+	}
 }
